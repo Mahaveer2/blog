b2eca208014e4db6fdcaf369e233a63e627f55de
Partab Rai <amrani.partab@gmail.com>
2023-01-11 00:01:10 +0500

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import NextNProgress from 'nextjs-progressbar';
 import '../styles/globals.scss';
 import { Layout } from '../components';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <NextNProgress color="#ffcc00"/>
       <Component {...pageProps} />
     </Layout>
   );
