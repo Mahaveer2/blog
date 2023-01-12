09fece2694b19b7c6c54a56c7c5e456a16ac933b
Partab Rai <amrani.partab@gmail.com>
2023-01-12 11:47:05 +0500

    ads

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import cript from 'next/script';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -11,8 +12,8 @@ class MyDocument extends Document {
       <Html>
         <Head>
         <meta name="google-site-verification" content="sIb_6pn3SEuVd-r3vJnNpuHXaq4l7FVxU_5I35LE5_k" />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9486411437483012"
-     crossorigin="anonymous"></script>
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9486411437483012"
+     crossorigin="anonymous"></Script>
           {/* <meta name="viewport" content="width=device-width,minimum-scale=1, initial-scale=1" /> */}
         </Head>
         <body>
